0b4cff5dd30430d78c0af43d5663bb3fb282bace
Stefan Mueller <stefan.mueller@gesis.org>
2016-10-27 11:20:32 +0200

    added shapefile downloads

--- R/download.census.R
+++ R/download.census.R
@@ -2,6 +2,7 @@
 #'
 #'@export
 
+# download census 1km grid data ------------------------------------------------
 download.census.1km <- function(data.path = "."){
   download.file(url = paste("https://www.zensus2011.de/SharedDocs/Downloads/",
                             "DE/Pressemitteilung/DemografischeGrunddaten/",
@@ -13,13 +14,18 @@ download.census.1km <- function(data.path = "."){
                                  sep = ""),
                 method = "auto",
                 mode = "wb")
+
+  # extract downloaded zip file ------------------------------------------------
   unzip(paste(data.path, "/", "Zensusatlas_spitze_Werte_1km_Gitter.zip",
               sep = ""),
         exdir = paste(data.path))
+
+  # remove downloaded zip file -------------------------------------------------
   file.remove(paste(data.path, "/", "Zensusatlas_spitze_Werte_1km_Gitter.zip",
                     sep = ""))
 }
 
+# download census 100m grid data -----------------------------------------------
 download.census.100m <- function(data.path = "."){
   download.file(url = paste("https://www.zensus2011.de/SharedDocs/Downloads/",
                             "DE/Pressemitteilung/DemografischeGrunddaten/",
@@ -31,9 +37,65 @@ download.census.100m <- function(data.path = "."){
                                  sep = ""),
                 method = "auto",
                 mode = "wb")
+
+  # extract downloaded zip file ------------------------------------------------
   unzip(paste(data.path, "/", "Bevoelkerung_100m_Gitter.zip",
               sep = ""),
         exdir = paste(data.path))
+
+  # remove downloaded zip file -------------------------------------------------
   file.remove(paste(data.path, "/", "Bevoelkerung_100m_Gitter.zip",
                     sep = ""))
+}
+
+# download inspire grid shapefile ----------------------------------------------
+download.inspire.grid <- function(data.path = "."){
+  download.file(url = paste("https://www.zensus2011.de/SharedDocs/Downloads/",
+                            "DE/Shapefile/Inspire.zip",
+                            "?__blob=publicationFile&v=5",
+                            sep = ""),
+                destfile = paste(data.path, "/",
+                                 "Inspire.zip",
+                                 sep = ""),
+                method = "auto",
+                mode = "wb")
+
+  # extract downloaded zip file ------------------------------------------------
+  unzip(paste(data.path, "/", "Inspire.zip",
+              sep = ""),
+        exdir = paste(data.path))
+
+  # remove downloaded zip file -------------------------------------------------
+  file.remove(paste(data.path, "/", "Inspire.zip",
+                    sep = ""))
+}
+
+# download shapefile for municipalties -----------------------------------------
+download.municipalties <- function(data.path = "."){
+  download.file(url = paste("https://www.zensus2011.de/SharedDocs/Downloads/",
+                            "DE/Shapefile/VG250_1Jan2011_UTM32.zip",
+                            "?__blob=publicationFile&v=25",
+                            sep = ""),
+                destfile = paste(data.path, "/",
+                                 "VG250_1Jan2011_UTM32.zip",
+                                 sep = ""),
+                method = "auto",
+                mode = "wb")
+
+  # extract downloaded zip file ------------------------------------------------
+  unzip(paste(data.path, "/", "VG250_1Jan2011_UTM32.zip",
+              sep = ""),
+        exdir = paste(data.path))
+
+  # remove downloaded zip file -------------------------------------------------
+  file.remove(paste(data.path, "/", "VG250_1Jan2011_UTM32.zip",
+                    sep = ""))
+
+  # remove shapefiles of other boundaries --------------------------------------
+  unlink(paste(data.path, "/", "VG250_Bundeslaender*",
+              sep = ""))
+  unlink(paste(data.path, "/", "VG250_Verwaltungsgemeinschaften*",
+               sep = ""))
+  unlink(paste(data.path, "/", "VG250_Kreise*",
+               sep = ""))
 }
