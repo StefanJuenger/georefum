4c9b4bc0a95f81f35f6caa6188a1df28c55ec313
Stefan Mueller <stefan.mueller@gesis.org>
2016-10-27 13:22:15 +0200

    added detachement for datasets; started focal linking function

--- R/simple.linking.R
+++ R/simple.linking.R
@@ -54,18 +54,58 @@ simple.linking <- function(obj.name = "linked.data",
     # assign object name -------------------------------------------------------
     eval(parse(text = paste(obj.name, " <<- dat", sep = "")))
   }
+detach(census.attr)
+detach(random.coords)
 }
 
-#
-# simple.linking <- function(data.path,
-#                            coordinate.file,
-#                            create.raster = FALSE,
-#                            download.raster = FALSE,
-#                            neighbourhood.matrix = c(1, 1, 1,
-#                                                     1, 1, 1,
-#                                                     1, 1, 1),
-#                            fun = mean){
-#   data(census.attr)
-#
-#
-# }
+focal.linking <- function(obj.name = "linked.data",
+                                  download = FALSE,
+                                  data.path = ".",
+                                  rasterize = FALSE,
+                                  coords.file = FALSE,
+                                  create.raster = FALSE,
+                                  download.raster = FALSE,
+                                  focal.matrix = c(1, 1, 1,                                                                                                      1, 1, 1,
+                                                   1, 1, 1),
+                                  fun = "mean"){
+
+  # download census data from zensus2011.de if toggled -------------------------
+  if(download == TRUE){
+    cat("Downloading census data")
+    download.census.1km()
+    #download.census.100m()
+  } else{
+    data(census.attr)
+  }
+
+  # rasterize data? ------------------------------------------------------------
+  if(rasterize == TRUE){
+    rasterize.census(data.path)
+  }
+
+  # use example coordinates? ---------------------------------------------------
+  if(coords.file == FALSE){
+    data(random.coords)
+  } else{
+    coords <- read.table(coords.file, sep = ";", dec = ",", header = TRUE)
+    coords <- SpatialPointsDataFrame(coords[, c("x", "y" )], dat,
+                                     proj4string = CRS("+init=epsg:3035"))
+  }
+
+  # set missing values in census data ------------------------------------------
+  for(i in names(census.attr)){
+    eval(parse(text = paste("census.attr$", i, "[census.attr$", i,
+                            "== -1] <- NA", sep = "")))
+  }
+
+  # focal analyses -------------------------------------------------------------
+  if(fun == "mean"){
+    for(i in names(census.attr)){
+      if(!exists("dat"){
+
+      }
+    }
+
+  }
+
+}
