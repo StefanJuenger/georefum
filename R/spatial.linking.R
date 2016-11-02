7042c5a98784b87d9075c585638b8bd73bdbb194
Stefan Mueller <stefan.mueller@gesis.org>
2016-11-02 10:42:13 +0100

    added missings with -9; also function for focal analyses

--- R/spatial.linking.R
+++ R/spatial.linking.R
@@ -38,6 +38,8 @@ simple.clinking <- function(obj.name = "linked.data",
   for(i in names(census.attr)){
     eval(parse(text = paste("census.attr$", i, "[census.attr$", i,
                             "== -1] <- NA", sep = "")))
+    eval(parse(text = paste("census.attr$", i, "[census.attr$", i,
+                            "[] == -9] <- NA", sep = "")))
   }
 
   # link coordinates with census data ------------------------------------------
@@ -55,23 +57,22 @@ simple.clinking <- function(obj.name = "linked.data",
     # assign object name -------------------------------------------------------
     eval(parse(text = paste(obj.name, " <<- dat", sep = "")))
   }
-
   # detach no longer needed internal datasets
   detach(census.attr)
   detach(random.coords)
 }
 
-# linking function that support focal analyses ---------------------------------
+# linking function that supports focal analyses --------------------------------
 focal.clinking <- function(obj.name = "linked.data",
-                                  download = FALSE,
-                                  data.path = ".",
-                                  rasterize = FALSE,
-                                  coords.file = FALSE,
-                                  create.raster = FALSE,
-                                  download.raster = FALSE,
-                                  focal.matrix = c(1, 1, 1,                                                                                                      1, 1, 1,
-                                                   1, 1, 1),
-                                  fun = "mean"){
+                           download = FALSE,
+                           data.path = ".",
+                           rasterize = FALSE,
+                           coords.file = FALSE,
+                           create.raster = FALSE,
+                           download.raster = FALSE,
+                           focal.matrix = matrix(c(1, 1, 1,                                                                                                  1, 1, 1,
+                                                   1, 1, 1), nr = 3, nc = 3),
+                           fun = "mean"){
 
   # download census data from zensus2011.de if toggled -------------------------
   if(download == TRUE){
@@ -99,17 +100,39 @@ focal.clinking <- function(obj.name = "linked.data",
   # set missing values in census data ------------------------------------------
   for(i in names(census.attr)){
     eval(parse(text = paste("census.attr$", i, "[census.attr$", i,
-                            "== -1] <- NA", sep = "")))
+                            "[] == -1] <- NA", sep = "")))
+    eval(parse(text = paste("census.attr$", i, "[census.attr$", i,
+                            "[] == -9] <- NA", sep = "")))
   }
 
   # focal analyses -------------------------------------------------------------
   if(fun == "mean"){
     for(i in names(census.attr)){
-      if(!exists("dat"){
-       fds
+      if(!exists("dat")){
+        eval(parse(text = paste("dat <- data.frame(", i,
+                                ".mean = ",
+                                "SDMTools::extract.data(random.coords@coords, ",
+                                "raster::focal(census.attr$", i,
+                                ", w = focal.matrix, ",
+                                "fun = ",
+                                "function(x){mean(x[-which(is.na(x))])})))",
+                                sep = "")))
+      } else{
+        eval(parse(text = paste("dat <- data.frame(cbind(dat, ", i,
+                                ".mean = ",
+                                "SDMTools::extract.data(random.coords@coords, ",
+                                "raster::focal(census.attr$", i,
+                                ", w = focal.matrix, ",
+                                "fun = ",
+                                "function(x){mean(x[-which(is.na(x))])}))))",
+                                sep = "")))
       }
+      eval(parse(text = paste(obj.name, " <<- dat", sep = "")))
     }
-
   }
 
+  # detach no longer needed internal datasets
+  detach(census.attr)
+  detach(random.coords)
 }
+
