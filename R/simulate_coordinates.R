fde394b606390a628eee91d258fc5f09144dfe05
Stefan Mueller <stefan.mueller@gesis.org>
2016-11-15 14:49:53 +0100

    added cologne and munich into the chosen municipalities

--- R/simulate_coordinates.R
+++ R/simulate_coordinates.R
@@ -1,69 +1,58 @@
 # simulate_coordinates.R
-#'
+#' Simulate coordinates
+#'@param mun.N Number of municipalties that should be sampled
+#'@param resp.N Number of respondents that should be sampled within the already
+#'sampled municipalities
 #'@export
 
-simulate_coordinates <- function(mun.N = 100,
-                                 resp.N = 100){
+simulate_coordinates <- function(mun.N = 98,
+                                 resp.N = 100) {
 
-  # tmp
-  setwd("C:/Users/mueller2/Desktop/")
-
-  # load municipalties shapefile -i----------------------------------------------
-  municipalities <- rgdal::readOGR("VG250_Gemeinden.shp", "VG250_Gemeinden",
-                                   encoding = "UTF-8")
-
-  # transform to EPSG:3035 -----------------------------------------------------
-  municipalities <- sp::spTransform(municipalities, '+init=epsg:3035')
-
-  # load inhabitants data ------------------------------------------------------
-  # inhab <- read.table(paste("C:/Users/mueller2/Desktop/csv_Bevoelkerung/",
-  #                           "Zensus11_Datensatz_Bevoelkerung.csv", sep = ""),
-  #                     sep = ";",
-  #                     dec = ",", header = TRUE)[, c("AGS_12", "AEWZ")]
-  #
-  # names(inhab)[names(inhab) == "AGS_12"] <- "RS"
-  #
-  # inhab <- inhab[!(nchar(inhab$RS) < 11),]
-  #
-  #
-  # stringr::str_pad(inhab$RS, 12, pad = "0")
-  # stringr::str_pad(municipalities@data$RS, 12, pad = "0")
-
-  # # merge with municipalties ---------------------------------------------------
-  # municipalities.bla <- sp::merge(municipalities, inhab, by = "RS",
-  #                                 all.x = FALSE)
+  # load census shape for sampling ---------------------------------------------
+  data(census.shapes)
 
   # draw random sample of municipalities ---------------------------------------
-  municipalities.random <- municipalities[sample(nrow(municipalities), mun.N),]
-
-  # municipalities$weights <- municipalities$AEWZ / 80000000
-
-  # # weighted
-  # municipalities.random <- municipalities[sample(nrow(municipalities), mun.N,
-  #                                                prob = (municipalities$AEWZ /
-  #                                                  80000000)),]
+  municipalities.random <- sp::rbind.SpatialPolygonsDataFrame(
+    census.shapes$municipalities[
+      sample(nrow(census.shapes$municipalities), mun.N),],
+    census.shapes$municipalities[
+      census.shapes$municipalities@data$GEN == "Köln" |
+        census.shapes$municipalities@data$GEN == "München",])
 
   # draw random sample of random municipalities --------------------------------
-  for(i in 1:nrow(municipalities.random)){
+  for (i in 1:nrow(municipalities.random)) {
 
     # check whether coordinates object already exists --------------------------
-    if(!exists("k")){
+    if (!exists("k")) {
 
       # draw sample ------------------------------------------------------------
       k <- sp::spsample(municipalities.random[i,], 100, type = "random")
     }
 
     # if coordinates object already exists -------------------------------------
-    else{
+    else {
 
       # draw sample ------------------------------------------------------------
-      k <- rbind(k, sp::spsample(municipalities.random[i,], 100,
-                                 type = "random"))
+      k <- sp::rbind.SpatialPoints(k, sp::spsample(municipalities.random[i,],
+                                                   100, type = "random"))
     }
   }
 
   # assign random id to every case ---------------------------------------------
+  data(key.dat)
 
+  k <- sp::SpatialPointsDataFrame(coords = k@coords,
+                                    data = as.data.frame(key.dat$id2),
+                                    proj4string = sp::CRS('+init=epsg:3035'))
+  names(k) <- "id2"
+
+  # remove no longer needed internal datasets ----------------------------------
+  if (exists("key.dat")) {
+    rm("key.dat", envir = globalenv())
+  }
+  if(exists("census.shapes")) {
+    rm("census.shapes", envir = globalenv())
+  }
 
   # return object --------------------------------------------------------------
   return(k)
