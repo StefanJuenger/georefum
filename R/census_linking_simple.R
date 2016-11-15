549ba4854932644109f732662b44884367e2496c
Stefan Mueller <stefan.mueller@gesis.org>
2016-11-15 14:49:23 +0100

    fixed SDMTools extract.data error

--- R/census_linking_simple.R
+++ R/census_linking_simple.R
@@ -9,48 +9,51 @@
 #' @param coords.file Path to file with coordinates; must be formatted as
 #' csv text file with a 'X' andheader...
 #' @return A \code{data.frame} with census attributes for each coordinate
+#' @importFrom SDMTools extract.data
 #'@export
 
 census_linking_simple <- function(download = FALSE,
                                   own.data = FALSE,
                                   data.path = ".",
-                                  coords.file = ""){
+                                  coords.file = "") {
 
   # case: census data should be downloaded -------------------------------------
-  if(download == TRUE){
+  if (download == TRUE) {
     cat("Downloading census data")
-    download.census.1km()
+    georefum::download.census.1km()
     #download.census.100m()
 
     # rasterize downloaded data ------------------------------------------------
-    rasterize.census(data.path)
+    georefum::rasterize.census(data.path)
   }
 
   # case: own, already downloaded census data should be used -------------------
-  if(own.data == TRUE){
-    rasterize.census(data.path)
+  if (own.data == TRUE) {
+    georefum::rasterize.census(data.path)
   }
 
   # case: internal, already downloaded census data should be used --------------
-  if(download == FALSE & own.data == FALSE){
+  if (download == FALSE & own.data == FALSE) {
     data(census.attr)
   }
 
   # set missing values in census data ------------------------------------------
-  for(i in names(census.attr)){
+  for (i in names(census.attr)) {
+
     eval(
       parse(
-        text = paste("census.attr$", i, "[census.attr$", i, "[] <= -1] <- NA",
-                     sep = "")))
+        text = paste("census.attr$", i, " @data@values", "[census.attr$", i,
+                     "@data@values <= -1] <- NA", sep = "")))
   }
 
+
   # case: use random example coordinates ---------------------------------------
-  if(coords.file == ""){
+  if (coords.file == "") {
     data(random.coords)
   }
 
   # case: use own coordinates --------------------------------------------------
-  else{
+  else {
     coords <- read.table(coords.file, sep = ";", dec = ",", header = TRUE)
     coords <- SpatialPointsDataFrame(coords[, c("x", "y" )], dat,
                                      proj4string = CRS("+init=epsg:3035"))
@@ -59,13 +62,13 @@ census_linking_simple <- function(download = FALSE,
   # link coordinates with census data ------------------------------------------
 
   # case: no own coordinates file was provided ---------------------------------
-  if(coords.file == ""){
+  if (coords.file == "") {
 
     # run linking on all census attributes -------------------------------------
-    for(i in names(census.attr)){
+    for (i in names(census.attr)) {
 
       # case: object does not exist yet ----------------------------------------
-      if(!exists("dat")){
+      if (!exists("dat")) {
         eval(
           parse(
             text = paste("dat <- data.frame(", i,
@@ -74,7 +77,7 @@ census_linking_simple <- function(download = FALSE,
       }
 
       # case: object already exists --------------------------------------------
-      else{
+      else {
         eval(
           parse(
             text = paste("dat <- data.frame(cbind(dat,", i,
@@ -85,13 +88,13 @@ census_linking_simple <- function(download = FALSE,
   }
 
   # case: use own coordinates --------------------------------------------------
-  else{
+  else {
 
     # run linking on all census attributes -------------------------------------
-    for(i in names(census.attr)){
+    for (i in names(census.attr)) {
 
       # case: object does not exist yet ----------------------------------------
-      if(!exists("dat")){
+      if (!exists("dat")) {
         eval(
           parse(
             text = paste("dat <- data.frame(", i,
@@ -100,7 +103,7 @@ census_linking_simple <- function(download = FALSE,
       }
 
       # case: object already exists --------------------------------------------
-      else{
+      else {
         eval(
           parse(
             text = paste("dat <- data.frame(cbind(dat,", i,
@@ -111,10 +114,10 @@ census_linking_simple <- function(download = FALSE,
   }
 
   # remove no longer needed internal datasets ----------------------------------
-  if(exists("census.attr")){
+  if (exists("census.attr")) {
     rm("census.attr", envir = globalenv())
   }
-  if(exists("random.coords")){
+  if (exists("random.coords")) {
     rm("random.coords", envir = globalenv())
   }
 
