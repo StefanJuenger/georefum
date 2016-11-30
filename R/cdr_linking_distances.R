0d96c9849add55f7bc420f3503c167b306de47ff
Stefan Mueller <stefan.mueller@gesis.org>
2016-11-30 15:24:56 +0100

    init distance calculation function (cdr)

---
+++ R/cdr_linking_distances.R
@@ -0,0 +1,7 @@
+# cdr_linking_distances.R
+#' Link Coordinates to data downloaded from the CDR by calculating distances
+#' @param file
+#' @param coords.file Path to file with coordinates; must be formatted as
+#' csv text file with a 'X' andheader...
+#' @return A \code{data.frame} with census attributes for each coordinate
+#'@export
