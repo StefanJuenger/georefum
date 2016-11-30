0c03d3cdcc86a8266c3235d4d278d49ea29bb9cf
Stefan Mueller <stefan.mueller@gesis.org>
2016-11-30 15:21:44 +0100

    added functionality to sample from certain municipalties

--- R/simulate_coordinates.R
+++ R/simulate_coordinates.R
@@ -5,36 +5,77 @@
 #'sampled municipalities
 #'@export
 
-simulate_coordinates <- function(mun.N = 98,
-                                 resp.N = 100) {
+simulate_coordinates <- function(mun.N = 100,
+                                 resp.N = 100,
+                                 mun = "") {
 
   # load census shape for sampling ---------------------------------------------
   data(census.shapes)
 
-  # draw random sample of municipalities ---------------------------------------
-  municipalities.random <- sp::rbind.SpatialPolygonsDataFrame(
-    census.shapes$municipalities[
-      sample(nrow(census.shapes$municipalities), mun.N),],
-    census.shapes$municipalities[
-      census.shapes$municipalities@data$GEN == "Köln" |
-        census.shapes$municipalities@data$GEN == "München",])
+  # case: pure random sample ---------------------------------------------------
+  if (mun == "") {
 
-  # draw random sample of random municipalities --------------------------------
-  for (i in 1:nrow(municipalities.random)) {
+    # draw random sample of municipalities -------------------------------------
+    municipalities.random <- sp::rbind.SpatialPolygonsDataFrame(
+      census.shapes$municipalities[
+        sample(nrow(census.shapes$municipalities), mun.N),],
+      census.shapes$municipalities[
+        census.shapes$municipalities@data$GEN == "Köln" |
+          census.shapes$municipalities@data$GEN == "München",])
 
-    # check whether coordinates object already exists --------------------------
-    if (!exists("k")) {
+    # draw random sample of random municipalities ------------------------------
+    for (i in 1:nrow(municipalities.random)) {
 
-      # draw sample ------------------------------------------------------------
-      k <- sp::spsample(municipalities.random[i,], 100, type = "random")
+      # check whether coordinates object already exists ------------------------
+      if (!exists("k")) {
+
+        # draw sample ----------------------------------------------------------
+        k <- sp::spsample(municipalities.random[i,], 100, type = "random")
+      }
+
+      # if coordinates object already exists -----------------------------------
+      else {
+
+        # draw sample ----------------------------------------------------------
+        k <- sp::rbind.SpatialPoints(k, sp::spsample(municipalities.random[i,],
+                                                     100, type = "random"))
+      }
+    }
+  }
+
+  # case: random sample from selected municipalities ---------------------------
+  else {
+
+    for (i in 1:length(mun)) {
+      print(mun[i])
+      if (!exists("municipalities.random")) {
+        municipalities.random <- census.shapes$municipalities[
+          census.shapes$municipalities@data$GEN == mun[i],]
+      } else {
+        municipalities.random <- sp::rbind.SpatialPolygonsDataFrame(
+          municipalities.random,
+          census.shapes$municipalities[
+            census.shapes$municipalities@data$GEN == mun[i],])
+      }
     }
 
-    # if coordinates object already exists -------------------------------------
-    else {
+    # draw random sample of random municipalities ------------------------------
+    for (i in 1:nrow(municipalities.random)) {
 
-      # draw sample ------------------------------------------------------------
-      k <- sp::rbind.SpatialPoints(k, sp::spsample(municipalities.random[i,],
-                                                   100, type = "random"))
+      # check whether coordinates object already exists ------------------------
+      if (!exists("k")) {
+
+        # draw sample ----------------------------------------------------------
+        k <- sp::spsample(municipalities.random[i,], 100, type = "random")
+      }
+
+      # if coordinates object already exists -----------------------------------
+      else {
+
+        # draw sample ----------------------------------------------------------
+        k <- sp::rbind.SpatialPoints(k, sp::spsample(municipalities.random[i,],
+                                                     100, type = "random"))
+      }
     }
   }
 
@@ -42,8 +83,8 @@ simulate_coordinates <- function(mun.N = 98,
   data(key.dat)
 
   k <- sp::SpatialPointsDataFrame(coords = k@coords,
-                                    data = as.data.frame(key.dat$id2),
-                                    proj4string = sp::CRS('+init=epsg:3035'))
+                                  data = as.data.frame(key.dat$id2),
+                                  proj4string = sp::CRS('+init=epsg:3035'))
   names(k) <- "id2"
 
   # remove no longer needed internal datasets ----------------------------------
