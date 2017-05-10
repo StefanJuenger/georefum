86cd4b470413cbdd327e09854585182cf41853f6
mueller2 <stefan.mueller@gesis.org>
2017-05-10 14:00:13 +0200

    added creation of missing values to be more 'realistic'

--- R/simulate_surveydata.R
+++ R/simulate_surveydata.R
@@ -1,9 +1,10 @@
 # simulate_surveydata.R
 #' Simulate survey data
 #'@param N Number of respondents that should be simulated
+#'@param na Number of missing values in attributes
 #'@export
 
-simulate_surveydata <- function(N = 10000) {
+simulate_surveydata <- function(N = 10000, na = 500) {
 
   # sinulate survey data attributes --------------------------------------------
   return(
@@ -20,17 +21,20 @@ simulate_surveydata <- function(N = 10000) {
       gender = sample(1:2, 10000, replace = TRUE, prob = c(.5, .5)),
 
       # att1 ---------------------------------------------------------------
-      att1 = sample(1:5, N, replace = TRUE,
-                    prob = c(.4, .2, .3, .05, .05)),
+      att1 = {attr <- sample(1:5, N, replace = TRUE,
+                             prob = c(.4, .2, .3, .05, .05));
+      attr[sample(1:N, na)] <- NA; attr},
 
       # att2 ---------------------------------------------------------------
-      att2 = sample(1:5, N, replace = TRUE,
-                    prob = c(.2, .2, .2, .3, .1)),
+      att2 = {attr <- sample(1:5, N, replace = TRUE,
+                    prob = c(.2, .2, .2, .3, .1));
+      attr[sample(1:N, na)] <- NA; attr},
 
       # att3 ---------------------------------------------------------------
-      att3 = sample(1:10, N, replace = TRUE,
+      att3 = {attr <- sample(1:10, N, replace = TRUE,
                     prob = c(.05, .1, .4, .1, .05, .05, .1, .025, .025,
-                             .05))
+                             .05));
+      attr[sample(1:N, na)] <- NA; attr}
     )
   )
 }
